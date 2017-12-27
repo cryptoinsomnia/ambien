3f4cfcf8bf222762d207c84227089c5dcc95c8e6
Francesco Agosti <francesco.agosti93@gmail.com>
2017-12-27 19:21:35 +0100

    Testing prettier

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React from 'react';
-import { Button } from 'antd';
 import { pure, compose, withState, withHandlers } from 'recompose';
+import { Button } from 'antd';
 
 const App = ({ isShowing, show, hide }) => (
   <div>
     <Button onClick={isShowing ? hide : show}> Toggle </Button>
-    {isShowing && 
-      <div>
-        YO
-      </div>
-    }
+    {isShowing && <div>YO</div>}
   </div>
-)
+);
 
+const a = b => {
+  return 2 + 2;
+};
+a();
 export default compose(
   pure,
   withState('isShowing', 'setShowing', false),
   withHandlers({
     show: ({ setShowing }) => () => setShowing(true),
-    hide: ({ setShowing }) => () => setShowing(false),
+    hide: ({ setShowing }) => () => setShowing(false)
   })
 )(App);
