318daf5e79f182f49f8486edbd124d8164d64420
Francesco Agosti <francesco.agosti93@gmail.com>
2017-12-27 18:46:03 +0100

    Add antd dependency

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Button } from 'antd';
 
 class App extends Component {
   render() {
     return (
       <div>
         <header>
-          <img src={logo} alt="logo" />
           <h1 >Welcome to React</h1>
         </header>
         <p>
           To get started, edit <code>src/App.js</code> and save to reload.
+          <Button> wut a button</Button>
         </p>
       </div>
     );
