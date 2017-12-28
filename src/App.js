7c33cf993eb2c5e02e3b0938ae82bab08479860e
Francesco Agosti <francesco.agosti93@gmail.com>
2017-12-28 02:46:31 +0100

    Configure eslint and prettier

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+// @flow
 import React from 'react';
 import { pure, compose, withState, withHandlers } from 'recompose';
 import { Button } from 'antd';
 
-const App = ({ isShowing, show, hide }) => (
+type Props = {|
+  isShowing: boolean,
+  show: Function,
+  hide: Function
+|};
+
+const App = ({ isShowing, show, hide }: Props) => (
   <div>
-    <Button onClick={isShowing ? hide : show}> Toggle </Button>
+    <Button onClick={isShowing ? hide : show}>Toggle</Button>
     {isShowing && <div>YO</div>}
   </div>
 );
@@ -14,6 +21,6 @@ export default compose(
   withState('isShowing', 'setShowing', false),
   withHandlers({
     show: ({ setShowing }) => () => setShowing(true),
-    hide: ({ setShowing }) => () => setShowing(false),
+    hide: ({ setShowing }) => () => setShowing(false)
   })
 )(App);
