cef48413524e90027815559ec460f09df82b98a0
Francesco Agosti <francesco.agosti93@gmail.com>
2017-12-29 21:56:45 +0100

    Add Footer and Header skeleton

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,24 @@
 // @flow
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Layout } from 'antd';
 
 import Main from './Main';
+import Footer from './Footer';
+import Header from './Header';
+
+const { Content } = Layout;
 
 const App = () => (
-  <Router>
-    <div>
-      <Route exact path="/" component={Main} />
-      <Route path="/other" component={Main} />
-    </div>
-  </Router>
+  <Layout>
+    <Header />
+    <Router>
+      <Content>
+        <Route exact path="/" component={Main} />
+      </Content>
+    </Router>
+    <Footer />
+  </Layout>
 );
 
 export default App;
