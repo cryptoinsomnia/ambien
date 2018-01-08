f2f31490c23b2894bb51b57bf33194d471408b2e
Francesco Agosti <francesco.agosti93@gmail.com>
2018-01-07 20:21:15 -0800

    Integrate ApolloClient and query some sample data

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,30 +1,56 @@
 // @flow
 import React from 'react';
+import { graphql } from 'react-apollo';
+import { compose, branch, renderComponent } from 'recompose';
+import gql from 'graphql-tag';
 
 import List, { type SubmissionInfo } from './List';
 import { Box } from './Layout';
 
-const fakeData: Array<SubmissionInfo> = [
-  {
-    title: 'The Criminal Underworld Is Dropping Bitcoin for Another Currency',
-    url:
-      'https://www.bloomberg.com/news/articles/2018-01-02/criminal-underworld-is-dropping-bitcoin-for-another-currency',
-    submitter: 'fragosti',
-    comments: [],
-  },
-  {
-    title: 'Bitcoin Is The MySpace Of The Cryptocurrency World (Part 1)',
-    url:
-      'https://seekingalpha.com/article/4135035-bitcoin-myspace-cryptocurrency-world-part-1',
-    submitter: 'rgmvisser',
-    comments: [],
-  },
-];
+type Props = {|
+  submissions: Array<SubmissionInfo>,
+|};
 
-const Main = () => (
+// Stateless component for loading state (to avoid if-statement within Main component)
+const MainLoading = () => <Box mx={2}> ...Loading </Box>;
+
+const Main = ({ submissions }: Props) => (
   <Box mx={2}>
-    <List dataSource={fakeData} />
+    <List dataSource={submissions} />
   </Box>
 );
 
-export default Main;
+// The AllPosts graphql query
+const AllPosts = gql`
+  query AllPosts {
+    allPosts {
+      id
+      title
+      url
+      author {
+        username
+      }
+      comments {
+        id
+      }
+    }
+  }
+`;
+
+// graphql(Query) returns a Higher Order Component that injects the result of Query into the Component
+// to which it is applied.
+// Takes an options argument.
+const withData = graphql(AllPosts, {
+  props: ({ data: { loading, allPosts } }) => ({
+    isLoading: loading,
+    submissions: allPosts,
+  }),
+});
+
+// Display the MainLoading component when isLoading is true.
+const displayLoadingState = branch(
+  ({ isLoading }) => isLoading,
+  renderComponent(MainLoading)
+);
+
+export default compose(withData, displayLoadingState)(Main);
