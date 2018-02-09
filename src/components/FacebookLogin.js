6398088a0ed53b73d03a19b6e0eeb3007e65a0a6
matthewdschulman <matthewdschulman@gmail.com>
2018-02-09 09:54:48 -0800

    add facebook login button to modal. now make it functional

---
+++ src/components/FacebookLogin.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Button from './Button';
+
+const FacebookLogin = () => (
+  <Button size="medium" m={1}>
+    Login with Facebook
+  </Button>
+);
+
+export default FacebookLogin;
